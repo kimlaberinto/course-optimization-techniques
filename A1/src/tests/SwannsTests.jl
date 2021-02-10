09813bf205e1f7c264085081bfbb2298dc97160d
Kim Paolo Laberinto <git@kimlaberinto.com>
2021-02-10 00:04:28 -0600

    Add ErrorException test

--- A1/src/tests/SwannsTests.jl
+++ A1/src/tests/SwannsTests.jl
@@ -14,6 +14,10 @@ function parabola_up(x)
     return x^2
 end
 
+function parabola_down(x)
+    return -x^2
+end
+
 result = SwannsBracketingMethod(parabola_up, -10, 0.1)
 println(result)
 
@@ -22,3 +26,5 @@ println(result)
 
 result = SwannsBracketingMethod(parabola_up, 0, 0.1)
 println(result)
+
+@test_throws ErrorException SwannsBracketingMethod(parabola_down, 0, 0.1)
