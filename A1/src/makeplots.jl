2ca385822f564492e9db3fe89979e5a7f9c09973
Kim Paolo Laberinto <git@kimlaberinto.com>
2021-02-16 23:08:16 -0600

    Add makeplots.jl for report plots

---
+++ A1/src/makeplots.jl
@@ -0,0 +1,43 @@
+using Revise
+
+using Plots
+using Printf
+
+includet("A1_module/A1Module.jl")
+using .A1Module
+
+const ROSENBROCK_A = 1.0
+const ROSENBROCK_B = 0.1
+
+N_f_eval = 0
+function rosenbrock_banana(x, y; a = ROSENBROCK_A, b = ROSENBROCK_B)
+    global N_f_eval += 1
+    return (a - x)^2 + 100 * b * (y - x^2)^2
+end
+
+
+begin 
+    x_plot = -2:0.01:2
+    y_plot = -2:0.01:3
+    contour(x_plot, y_plot, rosenbrock_banana, 
+        levels = 0:5:400, 
+        fill=false, 
+        label = "Rosenbrock",
+        c=cgrad(:thermal, scale=:exp, rev=false),
+        aspect_ratio = :equal,
+        size = (600, 600),
+        legend = :bottomright)
+    
+    title!("Example 1D Line Searches on Objective Function")
+
+    plot!([0, 2], [-2, 4], label="A", lw = 3)
+    plot!([-2, 4], [2, 0], label="B", lw = 3)
+    plot!([-2, 2], [-1, 0], label="C", lw = 3)
+    plot!([-2, 0], [0, 3], label="D", lw = 3)
+
+    scatter!([1], [1], label="Global Min", shape = :diamond, markersize = 10)
+
+    xlims!(-2, 2)
+    ylims!(-2, 3)
+
+end
