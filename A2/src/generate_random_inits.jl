c50e95dfe72b77a9fa0cae8160c25a3152516592
Kim Paolo Laberinto <git@kimlaberinto.com>
2021-03-10 13:19:43 -0600

    Add random inits

---
+++ A2/src/generate_random_inits.jl
@@ -0,0 +1,11 @@
+using Random
+
+const MIN_MAX_VAL = 4;
+function generate_random_inits()
+    rng = Random.MersenneTwister(1234);
+
+    random_array = (rand(rng, 4, 5) .* 2 .- 1) * MIN_MAX_VAL;
+    round.(random_array; digits = 2)
+end
+
+generate_random_inits()
