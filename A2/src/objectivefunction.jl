9ec71a8edfe3771d09da9aebf0d618353341a9e5
Kim Paolo Laberinto <git@kimlaberinto.com>
2021-03-09 20:47:39 -0600

    Add grads and hessians

--- A2/src/objectivefunction.jl
+++ A2/src/objectivefunction.jl
@@ -1,3 +1,15 @@
+module objectivefunctionModule
+
+using ForwardDiff
+
+export NDRosenbrock
+
+export analyticGradientNDRosenbrock
+export analyticHessianNDRosenbrock
+
+export autodiffGradientNDRosenbrock
+export autodiffHessianNDRosenbrock
+
 function NDRosenbrock(N::Integer, x::Array{T}) where T <: Real
     @assert length(x) == N
     @assert N >= 2
@@ -7,3 +19,40 @@ function NDRosenbrock(N::Integer, x::Array{T}) where T <: Real
     end
     return result
 end
+
+typeATerm(x_i) = 2*(1-x_i)*(-1)
+typeBTerm(x_i, x_next) = 100*2*(x_next - x_i^2)*(-2*x_i)
+typeCTerm(x_prev, x_i) = 100*2*(x_i - x_prev^2)
+function analyticGradientNDRosenbrock(N::Integer, x::Array{T}) where T <: Real
+    gradresult = zeros(eltype(x), N)
+
+    for i in 1:N
+        if i == 1 #First
+            gradresult[i] = 
+                typeATerm(x[i]) + typeBTerm(x[i], x[i+1])
+        elseif i == N #Last
+            gradresult[i] =
+                typeCTerm(x[i-1], x[i])
+        else #Intermediary terms
+            gradresult[i] = 
+                typeATerm(x[i]) + typeBTerm(x[i], x[i+1]) + typeCTerm(x[i-1], x[i])
+        end
+    end
+
+    return gradresult
+end
+
+function analyticHessianNDRosenbrock(N::Integer, x::Array{T}) where T <: Real
+    error("Currently undefined")
+end
+
+function autodiffGradientNDRosenbrock(N::Integer, x::Array{T}) where T <: Real
+    return ForwardDiff.gradient(in -> NDRosenbrock(N, in), x)
+end
+
+function autodiffHessianNDRosenbrock(N::Integer, x::Array{T}) where T <: Real
+    return ForwardDiff.hessian(in -> NDRosenbrock(N, in), x)
+end
+
+end
+
--- A2/src/tests/test_objfunction.jl
+++ A2/src/tests/test_objfunction.jl
@@ -1,6 +1,7 @@
 using Test
 
 include("../objectivefunction.jl")
+using .objectivefunctionModule
 
 @testset "Acceptable Inputs of Rosenbrock" begin
     @test_throws AssertionError NDRosenbrock(0, ones(2))
@@ -8,7 +9,27 @@ include("../objectivefunction.jl")
 end
 
 @testset "Zeros of Rosenbrock" begin
-    for i in 2:10
-        @test NDRosenbrock(i, ones(i)) == 0
+    for N in 2:10
+        @test NDRosenbrock(N, ones(N)) == 0
+    end
+end
+
+@testset "Gradients" begin
+    @testset "Gradients at origin" begin
+        for N in 2:10
+            @test isapprox(
+                analyticGradientNDRosenbrock(N, zeros(N)),
+                autodiffGradientNDRosenbrock(N, zeros(N)))
+        end
+    end
+end
+
+@testset "Hessians" begin
+    @testset "Hessians at origin" begin
+        for N in 2:10
+            @test isapprox(
+                analyticHessianNDRosenbrock(N, zeros(N)),
+                autodiffHessianNDRosenbrock(N, zeros(N)))
+        end
     end
 end
