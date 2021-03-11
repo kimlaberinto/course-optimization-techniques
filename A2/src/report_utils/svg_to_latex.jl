36bc965286f0f30e6a1250e41f11d41a7ee623e8
Kim Paolo Laberinto <git@kimlaberinto.com>
2021-03-10 20:16:24 -0600

    Cite

--- A2/src/report_utils/svg_to_latex.jl
+++ A2/src/report_utils/svg_to_latex.jl
@@ -1,4 +1,6 @@
 cd("assets")
+#https://docs.julialang.org/en/v1/base/file/
+#https://discourse.julialang.org/t/delete-all-files-with-a-specific-extension-in-a-particular-directory/38764/3
 for (root, dirs, files) in walkdir(".")
     println("SVG Files in $root")
     svg_files = filter(x->endswith(x, ".svg"), files)
